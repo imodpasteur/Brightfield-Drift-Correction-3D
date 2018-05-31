faa256b37d541d29f4b9d2d73bab9dca44e3c104
Andrey Aristov <aaristov@pasteur.fr>
2018-05-31 15:13:48 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pydrift',
+    version='0.0.1',
+    packages=['', 'tests'],
+    url='https://gitlab.pasteur.fr/aaristov/Brightfield_Drift_Tracking_3D.git',
+    license='BSD',
+    author='Andrey Aristov',
+    author_email='aaristov@pasteur.fr',
+    description='Acquire bright field images along with the super resolution data and use it to track drift in 3D with nanometer precision!'
+)
