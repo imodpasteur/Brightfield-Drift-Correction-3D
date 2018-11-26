8ca6c79ea4591587c3aaffbd39056472cef6ec8f
aaristov <andrey.aristov@pasteur.fr>
2018-11-26 17:50:26 +0100

    raduis z 5 to 8

--- bfdc/xcorr.py
+++ bfdc/xcorr.py
@@ -68,7 +68,7 @@ def fit_gauss_3d(stack, radius_xy=4, radius_z=5, z_zoom=20, min_xcorr = 0.5, deb
 
     xy_proj = cut_stack.max(axis=0)
     #z_proj = cut_stack.max(axis=(1, 2))
-    z_proj = cut_stack[:,y_px].max(axis=1)
+    z_proj = cut_stack[:,r].max(axis=1) #ignore y
     # z_proj = cut_stack[:,r,r]
 
     #[(_min, _max, y, x, sig), good] = gaussfit.fitSymmetricGaussian(xy_proj,sigma=1)
