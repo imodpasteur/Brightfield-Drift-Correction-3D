d0e713609d53ca89ae9add28d9a039f40213b4e2
aaristov <andrey.aristov@pasteur.fr>
2018-12-19 17:05:15 +0100

    reformat plot fitted crosses at the projections in debug mode

--- bfdc/xcorr.py
+++ bfdc/xcorr.py
@@ -1,44 +1,44 @@
 import numpy as np
 import matplotlib as mpl
+
 mpl.use('TkAgg')
 import matplotlib.pyplot as plt
 from skimage.feature import match_template
 import logging
+
 logger = logging.getLogger(__name__)
-#logger.setLevel(logging.WARNING)
+# logger.setLevel(logging.WARNING)
 logger.setLevel(logging.DEBUG)
 
 
 class LowXCorr(Exception):
     pass
 
+
 class FitResult():
 
-    def __init__(self, 
-                x=None, 
-                y=None, 
-                z=None, 
-                good=False, 
-                result_fit=None, 
-                z_px=None):
-
+    def __init__(self,
+                 x=None,
+                 y=None,
+                 z=None,
+                 good=False,
+                 result_fit=None,
+                 z_px=None):
         self.x = x
         self.y = y
         self.z = z
         self.good = good
         self.result_fit = result_fit
         self.z_px = z_px
-    
+
     def __iter__(self):
         for a in (self.x, self.y, self.z, self.good, self.result_fit, self.z_px):
             yield a
-    
+
     def __repr__(self):
-        return 'x={}, y={}, z={}, good={}, result_fit={}, z_px={}'.format(self.x, 
-                            self.y, self.z, self.good, self.result_fit, self.z_px)
-
-
-
+        return 'x={}, y={}, z={}, good={}, result_fit={}, z_px={}'.format(self.x,
+                                                                          self.y, self.z, self.good, self.result_fit,
+                                                                          self.z_px)
 
 
 def get_abs_max(data):
@@ -51,25 +51,26 @@ def get_abs_max(data):
     return np.unravel_index(np.argmax(data), data.shape)
 
 
-def fit_gauss_3d(stack:np.ndarray, 
-                radius_xy:int=4, 
-                radius_z:int=8, 
-                z_zoom:int=20, 
-                min_xcorr:np.float=0.5, 
-                z_init:np.float=None, 
-                fit_init:list=None, 
-                debug=False) -> FitResult:
+def fit_gauss_3d(stack: np.ndarray,
+                 radius_xy: int = 4,
+                 radius_z: int = 8,
+                 z_zoom: int = 20,
+                 min_xcorr: np.float = 0.5,
+                 z_init: np.float = None,
+                 fit_init: list = None,
+                 debug=False) -> FitResult:
     """
     Detects maximum on the stack in 3D
     Fitting 2D gaussian in xy, 4-order polynomial in z
     Ouputs [x,y,z] in pixels
     """
-    
+
     from bfdc import gaussfit
 
     logger.debug(f'Start fit_gauss_3d with the stack shape zyx {stack.shape}')
     logger.debug(f'radius_xy={radius_xy}, radius_z={radius_z}, z_zoom={z_zoom}')
-    assert np.ndim(stack) == 3, logger.error(f'fit_gauss_3d: input stack shape is wrong, expected 3 dim, got {stack.shape}')
+    assert np.ndim(stack) == 3, logger.error(
+        f'fit_gauss_3d: input stack shape is wrong, expected 3 dim, got {stack.shape}')
 
     if False:
         plt.imshow(stack.max(axis=0))
@@ -80,7 +81,7 @@ def fit_gauss_3d(stack:np.ndarray,
     logger.debug(f'Got absolute maximum xyz (px) {(x_px, y_px, z_px )}')
     cc_value = np.max(stack)
     if cc_value < min_xcorr:
-        #raise(LowXCorr("fit_gauss_3d: Cross corellation value os too low!"))
+        # raise(LowXCorr("fit_gauss_3d: Cross corellation value os too low!"))
         logger.error("fit_gauss_3d: Cross corellation value os too low!")
         return FitResult()
     else:
@@ -99,16 +100,16 @@ def fit_gauss_3d(stack:np.ndarray,
         z_crop = (z_start, z_stop)
         logger.debug(f'Using z boundaries: z_start={z_start}, z_stop={z_stop}')
 
-    
     _, y_max, x_max = stack.shape
-    y1 = max(0, y_px - r) 
+    y1 = max(0, y_px - r)
     y2 = min(y_max, y_px + r)
-    x1 = max(0, x_px - r) 
+    x1 = max(0, x_px - r)
     x2 = min(x_max, x_px + r)
     cut_stack = stack[z_start:z_stop, y1:y2, x1:x2]
     logger.debug(f'After cutting x,y,z, we got cut_stack shape {cut_stack.shape}')
-    if cut_stack.shape != (z_stop - z_start, 2 * r , 2 * r  ):
-        logger.error(f'Wrong cut_stack shape: expected {(z_stop - z_start, 2 * r + 1, 2 * r +1 )}, got {cut_stack.shape}')
+    if cut_stack.shape != (z_stop - z_start, 2 * r, 2 * r):
+        logger.error(
+            f'Wrong cut_stack shape: expected {(z_stop - z_start, 2 * r + 1, 2 * r +1 )}, got {cut_stack.shape}')
         return FitResult()
 
     xy_proj = cut_stack.max(axis=0)
@@ -121,45 +122,48 @@ def fit_gauss_3d(stack:np.ndarray,
 
     # [(_min, _max, y, x, sig), good] = gaussfit.fitSymmetricGaussian(xy_proj,sigma=1)
     logger.debug('Fit gauss xy')
-    try:    
-        # [(_min, _max, y, x, _, _, _), good] = gaussfit.fitEllipticalGaussian(xy_proj)
-         [result_fit, good] = gaussfit.fitEllipticalGaussian3D(cut_stack, init=fit_init)
-         background, height, z, y, x, el_x, el_y, el_z, an_xy, an_yz, an_xz, ramp_x, ramp_y, ramp_z = result_fit
-        
+    try:
+        [(_min, _max, y, x, _, _, _), good] = gaussfit.fitEllipticalGaussian(xy_proj)
+        #[result_fit, good] = gaussfit.fitEllipticalGaussian3D(cut_stack, init=fit_init)
+        #background, height, z, y, x, el_x, el_y, el_z, an_xy, an_yz, an_xz, ramp_x, ramp_y, ramp_z = result_fit
+
     except Exception as e:
         logger.error(f'Error in gaussian fit: {e}')
-        logger.error(f'result: {result_fit}')
+        #logger.error(f'result: {result_fit}')
         return FitResult()
 
-    # zfitter = FitPoly1D(z_proj, zoom=20, radius=8)
-    # z = zfitter(plot=debug)
+    zfitter = FitPoly1D(z_proj, zoom=20, radius=8)
+    z = zfitter(plot=debug)
     logger.debug(f'raw xyz {np.round((x, y, z),2)}')
-    
+
     x_found = x - r + x_px
     y_found = y - r + y_px
     logger.debug(f'xy found: {np.round((x_found, y_found),2)}')
 
     z_found = z + z_start
-    
+
     if debug:
         # fitted_ellipsoid = gaussfit.ellipticalGaussian3dOnRamp(*result_fit)(*np.indices(cut_stack.shape))
 
         # fit_residue = cut_stack - fitted_ellipsoid
 
-        fig = plt.figure(dpi=72, figsize=(8,3))
-        
+        fig = plt.figure(dpi=72, figsize=(8, 3))
+
         fig.add_subplot(131)
         plt.imshow(xy_proj)
+        plt.plot(x,y,'r+')
         plt.title('xy')
         plt.colorbar()
 
         fig.add_subplot(132)
         plt.imshow(zx_proj)
+        plt.plot(x,z,'r+')
         plt.title('zx')
         plt.colorbar()
 
         fig.add_subplot(133)
         plt.imshow(zy_proj)
+        plt.plot(y,z,'r+')
         plt.title('zy')
         plt.colorbar()
         '''
@@ -192,22 +196,27 @@ def fit_gauss_3d(stack:np.ndarray,
         plt.imshow(fit_residue.max(axis=2))
         plt.title('residue zy')
         plt.colorbar()
+        
 
-        #fig.add_subplot(144)
-        #plt.plot(z_proj, 'o', label='cc curve')
-        #plt.plot(gaussfit.symmetricGaussian1DonRamp(*params)(np.arange(len(z_proj))), '--', label='gauss fit')
-        #plt.legend()
-
-        #fig.add_subplot(144)
-        #plt.plot(polyfit.x + z_start, z_proj, 'o', label='cc curve')
-        #plt.plot(polyfit.x_new + z_start, polyfit.ext_curve, '--', label='poly fit')
-        #plt.plot(polyfit.x_new[np.argmax(polyfit.ext_curve)]+ z_start, max(polyfit.ext_curve), 'o', label='z found')
-        #plt.legend()
+        fig.add_subplot(144)
+        plt.plot(z_proj, 'o', label='cc curve')
+        plt.plot(gaussfit.symmetricGaussian1DonRamp(*params)(np.arange(len(z_proj))), '--', label='gauss fit')
+        plt.legend()
+        
+        fig.add_subplot(144)
+        try:
+            plt.plot(zfitter.x + z_start, z_proj, 'o', label='cc curve')
+            plt.plot(zfitter.x_new + z_start, zfitter.ext_curve, '--', label='poly fit')
+            plt.plot(zfitter.x_new[np.argmax(zfitter.ext_curve)]+ z_start, max(zfitter.ext_curve), 'o', label='z found')
+            plt.legend()
+        except ValueError as e:
+            logger.error(f'Error in subplot 144: {e}')
         '''
         plt.tight_layout()
         plt.show()
 
     return FitResult(x_found, y_found, z_found, good, None, z_px)
+
 
 class FitPoly1D:
 
@@ -232,14 +241,14 @@ class FitPoly1D:
         elif peak == 'min':
             self.peak = np.argmin
         else:
-            raise(ValueError(f'peak value \'{self.peak}\' not recognized. Expected \'max\' or \'min\''))
+            raise (ValueError(f'peak value \'{self.peak}\' not recognized. Expected \'max\' or \'min\''))
 
         if radius: self.do_crop(radius)
         self.interpolate()
         self.fit_poly()
         self.detect_peak()
 
-    def do_crop(self,radius):
+    def do_crop(self, radius):
         logger.debug(f'FitPoly1D:do_crop')
         index = self.peak(self.curve)
         a = max(0, index - radius)
@@ -251,11 +260,11 @@ class FitPoly1D:
         logger.debug(f'FitPoly1D:interpolate')
         self.x_full = np.arange(len(self.curve_full))
         self.x = np.arange(self.crop, self.crop + len(self.curve))
-        self.x_new = np.linspace(self.crop, 
-                                len(self.curve) + self.crop, 
-                                num=self.zoom * len(self.curve), 
-                                endpoint=False,
-                                dtype='f')
+        self.x_new = np.linspace(self.crop,
+                                 len(self.curve) + self.crop,
+                                 num=self.zoom * len(self.curve),
+                                 endpoint=False,
+                                 dtype='f')
 
     def fit_poly(self):
         logger.debug(f'FitPoly1D:fit_poly')
@@ -283,7 +292,7 @@ class FitPoly1D:
         return self.subpx_fit
 
 
-def fit_z_MSE(frame:np.ndarray, template:np.ndarray, zoom:int, order:int=4, plot:bool=False) -> float:
+def fit_z_MSE(frame: np.ndarray, template: np.ndarray, zoom: int, order: int = 4, plot: bool = False) -> float:
     """
     Fits MSE between frame 2D and template 3D to find z minimum
     :frame: 2D ndarraay
@@ -303,7 +312,7 @@ def fit_z_MSE(frame:np.ndarray, template:np.ndarray, zoom:int, order:int=4, plot
     logger.debug(f'found z {z_px}')
     return z_px
 
-    
+
 def cc_template(image, template, plot=False):
     try:
         cc = match_template(image, template, pad_input=True)
@@ -323,8 +332,6 @@ def cc_template(image, template, plot=False):
         logging.error(f"Error in cc_template. Image shape {image.shape}, template shape {template.shape  }")
 
 
-
-
 def cc_stack(image, stack, plot=False):
     image = np.array(image - np.mean(image), dtype='f')
     stack = np.array(stack - np.mean(stack), dtype='f')
@@ -333,7 +340,7 @@ def cc_stack(image, stack, plot=False):
         out.append(cc_template(image, t))
     out = np.array(out)
     if plot:
-        fig = plt.figure(figsize=(6,2))
+        fig = plt.figure(figsize=(6, 2))
 
         fig.add_subplot(131)
         plt.imshow(out.max(axis=0))
